e6e748dc923954128b7b0a5b8a5077f6a625fe4b
ophicial-pauloski <pasukwo5@gmail.com>
2024-01-09 09:43:01 +0100

    fixed auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
 import { MobileSidebar } from "./MobileSideBar";
 
 const Navbar = () => {
   return (
     <div className="flex items-center bg-white h-[86px]">
       <MobileSidebar />
-      <div className="flex w-full justify-end mr-3">
+      {/* <div className="flex w-full justify-end mr-3">
         <UserButton afterSignOutUrl="/" />
-      </div>
+      </div> */}
     </div>
   );
 };
